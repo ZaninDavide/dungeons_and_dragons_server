bda03d810e0e2663d76be2397c8128f69494b4b9
ZaninDavide <zanindavide02@gmail.com>
2020-04-10 11:47:49 +0200

    Connection and disconnection working.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(cors())
 var http = require("http").createServer(app)
 var io = require("socket.io")(http)
 
-const { initGame, newPlayer } = require("./utilities")
+const { initGame, newPlayer, sendablePlayer } = require("./utilities")
 
 let game = initGame()
 
@@ -15,49 +15,56 @@ app.get("/", function(req, res) {
     res.send("'Dungueons and Dragons' is listening")
 })
 
+app.get("/players", function(req, res) {
+    res.send(game.players.map(sendablePlayer))
+})
 
-app.post("/resetGame", function(req, res) {
-    game = initGame()
-    res.send("The game was reset")
+app.get("/players", function(req, res) {
+    res.send(game.players.map(c => c.name))
 })
 
 io.on("connection", function(socket) {
-    if (socket.player_name === undefined) return
+    console.log("New connection")
 
     socket.on("join game", function(name) {
         // send game data to app
-        socket.emit("players", { players: game.players })
-        socket.emit("enemies", { enemies: game.enemies })
-        socket.emit("walls",   {   walls: game.walls   })
+        socket.emit("players", { players: game.players.map(sendablePlayer) })
+        // socket.emit("enemies", { enemies: game.enemies })
+        // socket.emit("walls",   {   walls: game.walls   })
 
         // create a new player
         let new_player = newPlayer(socket, name)
 
         socket.player_name = name
         
-        // tell everyone else a new player as connected
-        io.emit("new player", {
-            name: new_player.name,
-        })
-        
         // add this player to the players list
         game.players.push(new_player)
+
+        // tell everyone else a new player as connected
+        io.emit("players", {
+            players: game.players.map(sendablePlayer),
+        })
+
+        console.log("'" + name + "' joined the game")
     })
 
     socket.on("disconnect", function() {
         if (socket.player_name === undefined) return
 
         const player = game.players.filter(player => player.name === socket.player_name)[0]
-
-        // comunicate that player has disconnected
-        io.emit("player disconnect", {
-            name: player.name,
-        })
+        const name = player.name.toString()
 
         // remove player from game.players
         game.players = game.players.filter(
             player => player.name !== socket.player_name
         )
+
+        // comunicate that player has disconnected
+        io.emit("players", {
+            players: game.players.map(sendablePlayer),
+        })
+
+        console.log("'" + name + "' disconnected")
     })
 })
 
--- utilities.js
+++ utilities.js
@@ -2,17 +2,31 @@ function initGame(){
     return {
         players: [],
         enemies: [],
-        walls: [],
+          walls: [],
     }
 }
 
-function newPlayer(socket, name, max_hp = 15){
+function newPlayer(socket, name, max_hp = 15, x = 0, y = 0, color = "blue"){
     return {
         socket,
         name,
         max_hp: max_hp,
         hp: max_hp,
+        x,
+        y,
+        color,
     }
 }
 
-module.exports = { initGame, newPlayer }
+function sendablePlayer(player){
+    return {
+        name: player.name,
+        max_hp: player.max_hp,
+        hp: player.hp,
+        x: player.x,
+        y: player.y,
+        color: player.color,
+    }
+}
+
+module.exports = { initGame, newPlayer, sendablePlayer }
